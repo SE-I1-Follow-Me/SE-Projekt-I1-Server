e2c81480667b6d7b77c19b48aee09e43f04e3e68
Michael <117443616+s83875@users.noreply.github.com>
2023-06-23 19:20:00 +0200

    Update Controller.java
    Added delete Mapping Import

--- src/main/java/com/FollowMeServer/Controller/Controller.java
+++ src/main/java/com/FollowMeServer/Controller/Controller.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 
 import java.util.List;
 
